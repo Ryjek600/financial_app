536dc00bf5af7066f6d0683d9a53db7c7be434e6
Ryszard Piasecki <ryszardpiasecki299@gmail.com>
2024-10-09 15:55:40 +0200

    Update_0910

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 
-db = SQLAlchemy()
+from extensions import db, mail  # Importuj z extensions.py
+
 migrate = Migrate()
 
 def create_app():
@@ -12,22 +14,39 @@ def create_app():
     app.config.from_object('config.Config')  # Konfiguracja z pliku głównego
     app.config.from_pyfile('config.py')       # Konfiguracja z folderu `instance`
     
+    # Inicjalizacja bazy danych, migracji i maila
     db.init_app(app)
     migrate.init_app(app, db)
+    mail.init_app(app)
 
     with app.app_context():
         # Import routes
         from routes.main_routes import main
         app.register_blueprint(main)
 
-        # Create tables
-        db.create_all()
+        # Debugowanie: Sprawdź, czy tabele są obecne
+        try:
+            tables = db.engine.table_names()
+            print(f"Tables in the database: {tables}")
+        except Exception as e:
+            print(f"Error retrieving tables: {e}")
+
+        # Create tables if they don't exist
+        try:
+            db.create_all()
+            print("All tables created.")
+        except Exception as e:
+            print(f"Error creating tables: {e}")
 
     return app
 
-
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
+
+
+
+
 
 
 
--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 
 class Config:
-    # URI do bazy danych
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
-    
-    # Zmniejsza obciążenie związane z monitorowaniem zmian w bazie danych
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Inne ustawienia
+    MAIL_SERVER = 'smtp.sendgrid.net'  # Adres serwera SMTP
+    MAIL_PORT = 587  # Port serwera SMTP
+    MAIL_USERNAME = 'apikey'
+    MAIL_PASSWORD = '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'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    SECRET_KEY = 'site'
 
-    # Możesz dodać inne ustawienia konfiguracyjne tutaj
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'site')  # Klucz do sesji
     DEBUG = os.getenv('DEBUG', True)  # Ustawienia debugowania
