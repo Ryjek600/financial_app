2baccfbe952724601a618f499cb4410e37105730
Ryszard Piasecki <ryszardpiasecki299@gmail.com>
2024-10-10 18:51:35 +0200

    Dodano plik .gitignore oraz usunięto klucze API

--- app.py
+++ app.py
@@ -2,36 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from dotenv import load_dotenv
+import os
 
-from extensions import db, mail  # Importuj z extensions.py
-
+db = SQLAlchemy()
+mail = Mail()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    
-    # Ładowanie konfiguracji z folderu `instance`
-    app.config.from_object('config.Config')  # Konfiguracja z pliku głównego
-    app.config.from_pyfile('config.py')       # Konfiguracja z folderu `instance`
-    
-    # Inicjalizacja bazy danych, migracji i maila
+    load_dotenv()
+    app.config.from_object('config.Config')
+    app.config.from_pyfile('config.py')
     db.init_app(app)
     migrate.init_app(app, db)
     mail.init_app(app)
 
     with app.app_context():
-        # Import routes
         from routes.main_routes import main
         app.register_blueprint(main)
 
-        # Debugowanie: Sprawdź, czy tabele są obecne
         try:
             tables = db.engine.table_names()
             print(f"Tables in the database: {tables}")
         except Exception as e:
             print(f"Error retrieving tables: {e}")
 
-        # Create tables if they don't exist
         try:
             db.create_all()
             print("All tables created.")
@@ -43,6 +39,7 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
+
 
 
 
--- instance/config.py
+++ instance/config.py
@@ -1,16 +1,18 @@
 import os
 
 class Config:
-    # Inne ustawienia
-    MAIL_SERVER = 'smtp.sendgrid.net'  # Adres serwera SMTP
-    MAIL_PORT = 587  # Port serwera SMTP
+    MAIL_SERVER = 'smtp.sendgrid.net'
+    MAIL_PORT = 587
     MAIL_USERNAME = 'apikey'
-    MAIL_PASSWORD = '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'
+    MAIL_PASSWORD = os.getenv('SENDGRID_API_KEY')
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    SECRET_KEY = 'site'
-
+    SECRET_KEY = os.getenv('SECRET_KEY', 'site')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'site')  # Klucz do sesji
-    DEBUG = os.getenv('DEBUG', True)  # Ustawienia debugowania
+    DEBUG = os.getenv('DEBUG', True)
+
+    # Ustawienia dla Flask-Mail
+    MAIL_DEBUG = os.getenv('MAIL_DEBUG', 'False').lower() == 'true'  # Zmiana tutaj
+
+
